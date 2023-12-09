6db5c6a7b8530674c6f3b29f3c6cdfed611cc298
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-12-09 06:27:03 -0800

    Final commit: modified script

--- lists.c
+++ lists.c
@@ -2,12 +2,14 @@
 
 /**
  * add_node - adds a node to the start of the list
+ *
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
  *
  * Return: size of list
  */
+
 list_t *add_node(list_t **head, const char *str, int num)
 {
 	list_t *new_head;
@@ -35,12 +37,14 @@ list_t *add_node(list_t **head, const char *str, int num)
 
 /**
  * add_node_end - adds a node to the end of the list
+ *
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
  *
  * Return: size of list
  */
+
 list_t *add_node_end(list_t **head, const char *str, int num)
 {
 	list_t *new_node, *node;
@@ -75,11 +79,12 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 }
 
 /**
- * print_list_str - prints only the str element of a list_t linked list
+ * print_list_str - prints only the
+ * str element of a list_t linked list
  * @h: pointer to first node
- *
  * Return: size of list
  */
+
 size_t print_list_str(const list_t *h)
 {
 	size_t i = 0;
@@ -96,11 +101,13 @@ size_t print_list_str(const list_t *h)
 
 /**
  * delete_node_at_index - deletes node at given index
+ *
  * @head: address of pointer to first node
  * @index: index of node to delete
  *
  * Return: 1 on success, 0 on failure
  */
+
 int delete_node_at_index(list_t **head, unsigned int index)
 {
 	list_t *node, *prev_node;
@@ -137,9 +144,9 @@ int delete_node_at_index(list_t **head, unsigned int index)
 /**
  * free_list - frees all nodes of a list
  * @head_ptr: address of pointer to head node
- *
- * Return: void
+ * Return: nothing
  */
+
 void free_list(list_t **head_ptr)
 {
 	list_t *node, *next_node, *head;
